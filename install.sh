9eecf908647cbf82e33f13c4bdf573d5aeec73fc
Yamada Koji <kou64yama@gmail.com>
2021-02-06 14:40:00 +0900

    Add bootstrap code

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 {
   set -eo pipefail
 
+  trap 'rm -rf $temp' 0
+  temp=$(mktemp -d)
+
+  if [[ -z "$DOTFILES" ]]; then
+    curl -fsSL "https://github.com/kou64yama/dotfiles/archive/${GITHUB_SHA:-main}.tar.gz" |
+      tar --strip-components=1 -x -C "$temp"
+    exec DOTFILES="$temp" bash "$temp/install.sh"
+  fi
+
   : homebrew && {
     if ! command -v brew >/dev/null 2>&1; then
       cat >&2 <<EOF
