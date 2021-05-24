189c13802692fa6861d3d95b0667e73f2d5316cf
Yamada Koji <kou64yama@gmail.com>
2021-05-25 08:58:55 +0900

    :sparkles: Remove ~/go/bin from PATH

--- stage3/files/zsh/00-env.zsh
+++ stage3/files/zsh/00-env.zsh
@@ -4,4 +4,4 @@ eval "$(anyenv init - zsh)"
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
-PATH="$HOME/bin:$HOME/go/bin${PATH:+:$PATH}"
+PATH="$HOME/bin${PATH:+:$PATH}"
