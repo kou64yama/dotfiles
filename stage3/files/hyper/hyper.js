a03deeb59f1a4335af494a6892cae5870e50b691
Yamada Koji <kou64yama@gmail.com>
2021-05-25 08:57:34 +0900

    :sparkles: Update fontSize and fontFamily

--- stage3/files/hyper/hyper.js
+++ stage3/files/hyper/hyper.js
@@ -9,10 +9,10 @@ module.exports = {
     updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"Fira Code", "Migu 2M", "Noto Emoji", monospace',
+    fontFamily: '"FiraCode Nerd Font", "Migu 2M", "Noto Emoji", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: "normal",
