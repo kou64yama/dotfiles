8dd6300b8b207aac0d93e6775b7cf59b48ddcb76
Yamada Koji <1706782+kou64yama@users.noreply.github.com>
2026-06-14 23:43:04 +0900

    feat(nvim): add base options

---
+++ files/nvim/lua/config/options.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+
+vim.opt.clipboard:append("unnamedplus")
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+
+vim.cmd.colorscheme("habamax")
